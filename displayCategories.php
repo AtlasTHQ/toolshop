3e71fa259ddef5adfd97375bf8ac9a062188abcf
Halldór Þorri <halldoras206@nemi@tskoli.is>
2016-12-04 02:21:18 +0000

    minor edit displayCategories.php

--- displayCategories.php
+++ displayCategories.php
@@ -2,6 +2,7 @@
     $result = $conn->prepare("SELECT * FROM categories");
     $result->execute();
     while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+        $catID = $row["categoryID"];
         $catName = $row["categoryName"]; ?>
         <a href="#" class="list-group-item"><?php echo $catName ?></a>
 <?php } ?>

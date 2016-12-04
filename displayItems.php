dcc07f05ba2d156a124d78e5a4a2150504a25d9d
AtlasTHQ <atlasthq@gmail.com>
2016-12-04 04:08:05 +0000

    Categories now filter through item list

--- displayItems.php
+++ displayItems.php
@@ -1,25 +1,53 @@
 <?php
-    $catID = $_GET['categoryID'];
     $result = $conn->prepare("call getItemList");
     $result->execute();
-    while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        $itemID = $row["itemID"];
-        $imgPath = $row["itemImgPath"];
-        $itemName = $row["itemName"];
-        $itemDesc = $row["itemDescription"];
-        $itemPrice = $row["itemPrice"]; ?>
-    <div class="col-sm-4 col-lg-4 col-md-4">
-        <a href=<?php echo "item.php?itemID=$itemID" ?> class="displayItem">
-            <div class="thumbnail">
-                <div class="imageResizer">
-                    <img src="<?php echo $imgPath ?>">
+    if (!isset($_GET['categoryID'])) {
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $itemID = $row["itemID"];
+            $imgPath = $row["itemImgPath"];
+            $itemName = $row["itemName"];
+            $itemDesc = $row["itemDescription"];
+            $itemPrice = $row["itemPrice"]; 
+        ?>
+        <div class="col-sm-4 col-lg-4 col-md-4">
+            <a href=<?php echo "item.php?itemID=$itemID" ?> class="displayItem">
+                <div class="thumbnail">
+                    <div class="imageResizer">
+                        <img src="<?php echo $imgPath ?>">
+                    </div>
+                    <div class="caption">
+                        <h4 class="pull-right"><?php echo "$".$itemPrice ?></h4>
+                        <h4><?php echo $itemName ?></h4>
+                        <p><?php echo $itemDesc ?></p>
+                    </div>
                 </div>
-                <div class="caption">
-                    <h4 class="pull-right"><?php echo "$".$itemPrice ?></h4>
-                    <h4><?php echo $itemName ?></h4>
-                    <p><?php echo $itemDesc ?></p>
+            </a>
+        </div>
+        <?php } ?>
+    <?php } else {
+        $catID = $_GET['categoryID'];
+        $result = $conn->prepare("call getCategoryItem($catID)");
+        $result->execute();
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            $itemID = $row["itemID"];
+            $imgPath = $row["itemImgPath"];
+            $itemName = $row["itemName"];
+            $itemDesc = $row["itemDescription"];
+            $itemPrice = $row["itemPrice"]; ?>
+        <div class="col-sm-4 col-lg-4 col-md-4">
+            <a href=<?php echo "item.php?itemID=$itemID" ?> class="displayItem">
+                <div class="thumbnail">
+                    <div class="imageResizer">
+                        <img src="<?php echo $imgPath ?>">
+                    </div>
+                    <div class="caption">
+                        <h4 class="pull-right"><?php echo "$".$itemPrice ?></h4>
+                        <h4><?php echo $itemName ?></h4>
+                        <p><?php echo $itemDesc ?></p>
+                    </div>
                 </div>
-            </div>
-        </a>
-    </div>
+            </a>
+        </div>
+    <?php } ?>
 <?php } ?>
+        
